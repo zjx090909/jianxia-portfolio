235ba7db1d72b2dcc72ba83ff9bc2b9354a72c2f
Jianxia Zhang <zjx090909@gmail.com>
2025-10-06 19:38:19 -0400

    Fixed links

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,7 +3,7 @@ export default function Contact() {
 		<div className="flex flex-col items-center justify-center p-8 min-h-[60vh]">
 			<h1 className="text-4xl font-bold mb-6">Contact Me</h1>
 			<a href="mailto:zhangjianxia@example.com" className="text-brandBlue underline text-xl">
-				zhangjianxia@example.com
+				jess09zhan@gmail.com
 			</a>
 		</div>
 	)
